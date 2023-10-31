37e9347dedb4c28cc793a9a4e23baa3d88f2368d
Erick Del Piero <Erick.gozales.r@uni.pe>
2023-10-31 12:28:24 -0500

    Leyendo HTML desde JavaScript

--- Ejemplo/script.js
+++ Ejemplo/script.js
@@ -1,1 +1,9 @@
-console.log("Texto que se imprime en consola del navegador")
+const h1 = document.querySelector('h1');
+const p = document.querySelector('p');
+const color = document.querySelector('.ColorRojo');
+const id = document.querySelector('#Final');
+const input= document.querySelector('input');
+
+
+console.log(h1)                         //Solo imprime lo que hay en la etiqueta h1
+console.log([h1, p, color, id, input]); //Al imprimir así, podemos ver en la consola del navegador sus propiedades.
