42ea96284f6eec4789590664a95cbe2130eba643
Erick Del Piero <Erick.gozales.r@uni.pe>
2023-10-31 15:55:43 -0500

    addEventListener

--- Ejemplo/script.js
+++ Ejemplo/script.js
@@ -3,9 +3,14 @@ const input1 = document.querySelector('#Calculo1');
 const input2 = document.querySelector('#Calculo2');
 const btn=document.querySelector('#btnSumar');
 const R= document.querySelector('#Resultado');
+const Form= document.querySelector("#Formulario");
 
 
-function Click(){
+Form.addEventListener('submit', Click);             //creamos escuchador de eventos para el formulario
+
+
+function Click(event){
+    event.preventDefault();                         //Permite no recargar la página en el formulario
     const x=input1.value;
     const y=input2.value;
     const z=x+y;
